fdc1bc66dc98675ede3ac6760cee7699a30a8793
N1X <nixblr@gmail.com>
2023-08-02 20:57:23 +0300

    WebSocket support added

--- main/http_ap.c
+++ main/http_ap.c
@@ -1,6 +1,6 @@
 
 
-#include"http_ap.h"
+#include "http_ap.h"
 
 #include <esp_wifi.h>
 #include <esp_event.h>
@@ -14,27 +14,36 @@
 #include "esp_tls_crypto.h"
 #include <esp_http_server.h>
 #include "display.h"
-
 #include <string.h>
 
 static const char *TAG = "http_ap";
 
 extern const char index_html_start[] asm("_binary_index_html_start");
 extern const char index_html_end[] asm("_binary_index_html_end");
-
 extern const char style_start[] asm("_binary_style_css_start");
 extern const char style_end[] asm("_binary_style_css_end");
+
+struct async_resp_arg
+{
+    httpd_handle_t hd;
+    int fd;
+};
+static httpd_handle_t server = NULL;
+
+static esp_err_t trigger_async_send(httpd_handle_t handle, httpd_req_t *req);
 
 /* Style */
 static esp_err_t style_get_handler(httpd_req_t *req)
 {
-    char*  buf;
+    char *buf;
     size_t buf_len;
     const size_t style_len = style_end - style_start;
     buf_len = httpd_req_get_hdr_value_len(req, "Host") + 1;
-    if (buf_len > 1) {
+    if (buf_len > 1)
+    {
         buf = malloc(buf_len);
-        if (httpd_req_get_hdr_value_str(req, "Host", buf, buf_len) == ESP_OK) {
+        if (httpd_req_get_hdr_value_str(req, "Host", buf, buf_len) == ESP_OK)
+        {
             ESP_LOGI(TAG, "Found header => Host: %s", buf);
         }
         free(buf);
@@ -48,29 +57,21 @@ static esp_err_t style_get_handler(httpd_req_t *req)
 /* An HTTP GET handler */
 static esp_err_t index_get_handler(httpd_req_t *req)
 {
-    char*  buf;
+    char *buf;
     size_t buf_len;
     const size_t index_html_len = index_html_end - index_html_start;
-
-    ESP_LOGI(TAG, "Callback index!");
-
-    /* Get header value string length and allocate memory for length + 1,
-     * extra byte for null termination */
     buf_len = httpd_req_get_hdr_value_len(req, "Host") + 1;
-    if (buf_len > 1) {
+    if (buf_len > 1)
+    {
         buf = malloc(buf_len);
         /* Copy null terminated value string into buffer */
-        if (httpd_req_get_hdr_value_str(req, "Host", buf, buf_len) == ESP_OK) {
+        if (httpd_req_get_hdr_value_str(req, "Host", buf, buf_len) == ESP_OK)
+        {
             ESP_LOGI(TAG, "Found header => Host: %s", buf);
         }
         free(buf);
     }
-
-    /* Send response with custom headers and body set as the
-     * string passed in user context*/
-    //const char* resp_str = (const char*) req->user_ctx;
     httpd_resp_send(req, index_html_start, index_html_len);
-
     return ESP_OK;
 }
 
@@ -111,45 +112,134 @@ static esp_err_t set_post_handler(httpd_req_t *req)
     return ESP_OK;
 }
 
+static esp_err_t handle_ws_req(httpd_req_t *req)
+{
+    if (req->method == HTTP_GET)
+    {
+        ESP_LOGI(TAG, "Handshake done, the new connection was opened");
+        return ESP_OK;
+    }
+
+    httpd_ws_frame_t ws_pkt;
+    uint8_t *buf = NULL;
+    memset(&ws_pkt, 0, sizeof(httpd_ws_frame_t));
+    ws_pkt.type = HTTPD_WS_TYPE_TEXT;
+    esp_err_t ret = httpd_ws_recv_frame(req, &ws_pkt, 0);
+    if (ret != ESP_OK)
+    {
+        ESP_LOGE(TAG, "httpd_ws_recv_frame failed to get frame len with %d", ret);
+        return ret;
+    }
+
+    if (ws_pkt.len)
+    {
+        buf = calloc(1, ws_pkt.len + 1);
+        if (buf == NULL)
+        {
+            ESP_LOGE(TAG, "Failed to calloc memory for buf");
+            return ESP_ERR_NO_MEM;
+        }
+        ws_pkt.payload = buf;
+        ret = httpd_ws_recv_frame(req, &ws_pkt, ws_pkt.len);
+        if (ret != ESP_OK)
+        {
+            ESP_LOGE(TAG, "httpd_ws_recv_frame failed with %d", ret);
+            free(buf);
+            return ret;
+        }
+        ESP_LOGI(TAG, "Got packet with message: %s", ws_pkt.payload);
+    }
+
+    ESP_LOGI(TAG, "frame len is %d", ws_pkt.len);
+    DisplayShowMessage((char *) ws_pkt.payload, DSE_NONE, 1);
+
+    if (ws_pkt.type == HTTPD_WS_TYPE_TEXT &&
+        strcmp((char *)ws_pkt.payload, "toggle") == 0)
+    {
+        free(buf);
+        return trigger_async_send(req->handle, req);
+    }
+    free(buf);
+    return ESP_OK;
+}
+
+static void ws_async_send(void *arg)
+{
+    httpd_ws_frame_t ws_pkt;
+    struct async_resp_arg *resp_arg = arg;
+    httpd_handle_t hd = resp_arg->hd;
+    int fd = resp_arg->fd;
+
+    char buff[4];
+    memset(buff, 0, sizeof(buff));
+
+    memset(&ws_pkt, 0, sizeof(httpd_ws_frame_t));
+    ws_pkt.payload = (uint8_t *)buff;
+    ws_pkt.len = strlen(buff);
+    ws_pkt.type = HTTPD_WS_TYPE_TEXT;
+
+    static size_t max_clients = CONFIG_LWIP_MAX_LISTENING_TCP;
+    size_t fds = max_clients;
+    int client_fds[max_clients];
+
+    esp_err_t ret = httpd_get_client_list(server, &fds, client_fds);
+
+    if (ret != ESP_OK)
+    {
+        return;
+    }
+
+    for (int i = 0; i < fds; i++)
+    {
+        int client_info = httpd_ws_get_fd_info(server, client_fds[i]);
+        if (client_info == HTTPD_WS_CLIENT_WEBSOCKET)
+        {
+            httpd_ws_send_frame_async(hd, client_fds[i], &ws_pkt);
+        }
+    }
+    free(resp_arg);
+}
+
+static esp_err_t trigger_async_send(httpd_handle_t handle, httpd_req_t *req)
+{
+    struct async_resp_arg *resp_arg = malloc(sizeof(struct async_resp_arg));
+    resp_arg->hd = req->handle;
+    resp_arg->fd = httpd_req_to_sockfd(req);
+    return httpd_queue_work(handle, ws_async_send, resp_arg);
+}
+
 static const httpd_uri_t style = {
-    .uri       = "/style.css",
-    .method    = HTTP_GET,
-    .handler   = style_get_handler,
-    .user_ctx  = NULL
-};
-
+    .uri = "/style.css",
+    .method = HTTP_GET,
+    .handler = style_get_handler,
+    .user_ctx = NULL};
 static const httpd_uri_t hello = {
-    .uri       = "/",
-    .method    = HTTP_GET,
-    .handler   = index_get_handler,
-    .user_ctx  = NULL
-};
-
+    .uri = "/",
+    .method = HTTP_GET,
+    .handler = index_get_handler,
+    .user_ctx = NULL};
 static const httpd_uri_t hello_post = {
-    .uri       = "/set.html",
-    .method    = HTTP_POST,
-    .handler   = set_post_handler,
-    .user_ctx  = NULL
-};
+    .uri = "/set.html",
+    .method = HTTP_POST,
+    .handler = set_post_handler,
+    .user_ctx = NULL};
+httpd_uri_t ws = {
+    .uri = "/ws",
+    .method = HTTP_GET,
+    .handler = handle_ws_req,
+    .user_ctx = NULL,
+    .is_websocket = true};
 
-/* This handler allows the custom error handling functionality to be
- * tested from client side. For that, when a PUT request 0 is sent to
- * URI /ctrl, the /hello and /echo URIs are unregistered and following
- * custom error handler http_404_error_handler() is registered.
- * Afterwards, when /hello or /echo is requested, this custom error
- * handler is invoked which, after sending an error message to client,
- * either closes the underlying socket (when requested URI is /echo)
- * or keeps it open (when requested URI is /hello). This allows the
- * client to infer if the custom error handler is functioning as expected
- * by observing the socket state.
- */
 esp_err_t http_404_error_handler(httpd_req_t *req, httpd_err_code_t err)
 {
-    if (strcmp("/hello", req->uri) == 0) {
+    if (strcmp("/hello", req->uri) == 0)
+    {
         httpd_resp_send_err(req, HTTPD_404_NOT_FOUND, "/hello URI is not available");
         /* Return ESP_OK to keep underlying socket open */
         return ESP_OK;
-    } else if (strcmp("/echo", req->uri) == 0) {
+    }
+    else if (strcmp("/echo", req->uri) == 0)
+    {
         httpd_resp_send_err(req, HTTPD_404_NOT_FOUND, "/echo URI is not available");
         /* Return ESP_FAIL to close underlying socket */
         return ESP_FAIL;
@@ -167,57 +257,52 @@ static httpd_handle_t start_webserver(void)
 
     // Start the httpd server
     ESP_LOGI(TAG, "Starting server on port: '%d'", config.server_port);
-    if (httpd_start(&server, &config) == ESP_OK) {
+    if (httpd_start(&server, &config) == ESP_OK)
+    {
         // Set URI handlers
         ESP_LOGI(TAG, "Registering URI handlers");
         httpd_register_uri_handler(server, &style);
         httpd_register_uri_handler(server, &hello);
         httpd_register_uri_handler(server, &hello_post);
+        httpd_register_uri_handler(server, &ws);
         return server;
     }
-
     ESP_LOGI(TAG, "Error starting server!");
     return NULL;
 }
 
 static void stop_webserver(httpd_handle_t server)
 {
-    // Stop the httpd server
     httpd_stop(server);
 }
 
-static void disconnect_handler(void* arg, esp_event_base_t event_base,
-                               int32_t event_id, void* event_data)
+static void disconnect_handler(void *arg, esp_event_base_t event_base,
+                               int32_t event_id, void *event_data)
 {
-    httpd_handle_t* server = (httpd_handle_t*) arg;
-    if (*server) {
+    httpd_handle_t *server = (httpd_handle_t *)arg;
+    if (*server)
+    {
         ESP_LOGI(TAG, "Stopping webserver");
         stop_webserver(*server);
         *server = NULL;
     }
 }
 
-static void connect_handler(void* arg, esp_event_base_t event_base,
-                            int32_t event_id, void* event_data)
+static void connect_handler(void *arg, esp_event_base_t event_base,
+                            int32_t event_id, void *event_data)
 {
-    httpd_handle_t* server = (httpd_handle_t*) arg;
-    if (*server == NULL) {
+    httpd_handle_t *server = (httpd_handle_t *)arg;
+    if (*server == NULL)
+    {
         ESP_LOGI(TAG, "Starting webserver");
         *server = start_webserver();
     }
 }
 
-
 void httpap_init(void)
 {
-    static httpd_handle_t server = NULL;
-
-    /* Register event handlers to stop the server when Wi-Fi is disconnected,
-     * and re-start it upon connection.
-     */
     ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &connect_handler, &server));
     ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, &disconnect_handler, &server));
 
-    /* Start the server for the first time */
     server = start_webserver();
 }
